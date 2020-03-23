419d90d12b5e1f4024d96c75c19850efeb92a966
Hashem <hashemp206@yahoo.com>
2020-03-23 12:04:14 +0430

    Remove the generic error type from the 'LoadFeedResult' to simplify type constraints

--- EssentialFeed/FeedAPI/FeedItemMapper.swift
+++ EssentialFeed/FeedAPI/FeedItemMapper.swift
@@ -41,7 +41,7 @@ final class FeedItemMapper {
     
     static func map(_ data: Data, _ response: HTTPURLResponse) -> RemoteFeedLoader.Result {
         guard response.statusCode == OK_200, let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return .failure(.invalidData)
+            return .failure(RemoteFeedLoader.Error.invalidData)
         }
         
         return .success(root.feed)
--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -41,7 +41,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         let (sut, client) = makeSUT()
         let givenError = NSError(domain: "test", code: 0, userInfo: nil)
         
-        expect(sut, toCompleteWithResult: RemoteFeedLoader.Result.failure(.connectivity), when: {
+        expect(sut, toCompleteWithResult: RemoteFeedLoader.Result.failure(RemoteFeedLoader.Error.connectivity), when: {
             client.complete(with: givenError)
         })
     }
@@ -55,7 +55,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         samples.enumerated().forEach { (arg) in
             
             let (index, code) = arg
-            expect(sut, toCompleteWithResult: RemoteFeedLoader.Result.failure(.invalidData), when: {
+            expect(sut, toCompleteWithResult: RemoteFeedLoader.Result.failure(RemoteFeedLoader.Error.invalidData), when: {
                 let json = makeItemJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             })
@@ -77,7 +77,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         // given
         let (sut, client) = makeSUT()
         
-        expect(sut, toCompleteWithResult: RemoteFeedLoader.Result.failure(.invalidData), when: {
+        expect(sut, toCompleteWithResult: RemoteFeedLoader.Result.failure(RemoteFeedLoader.Error.invalidData), when: {
             let invalidJSON = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJSON)
         })
@@ -145,7 +145,7 @@ class RemoteFeedLoaderTests: XCTestCase {
             switch (receivedResult, expectedResult) {
                 case let (.success(receivedItems), .success(expectedItems)):
                     XCTAssertEqual(receivedItems, expectedItems, file: file, line: line)
-                case let (.failure(receivedError), .failure(expectedError)):
+                case let (.failure(receivedError as RemoteFeedLoader.Error), .failure(expectedError as RemoteFeedLoader.Error)):
                     XCTAssertEqual(receivedError, expectedError, file: file, line: line)
                 default:
                     XCTFail("Expected \(expectedResult) got \(receivedResult) instead", file: file, line: line)
