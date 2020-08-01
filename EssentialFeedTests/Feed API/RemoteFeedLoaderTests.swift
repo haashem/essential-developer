b489fedf763e36e0c9cca477377aadd026c649b7
Hashem <hashemp206@yahoo.com>
2020-08-01 14:07:29 +0400

    Rename 'RemoteFeedLoaderTests' to reflect the use case it refers to (Load Feed From Remote)

--- EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -10,7 +10,7 @@ import XCTest
 @testable import EssentialFeed
 
 
-class RemoteFeedLoaderTests: XCTestCase {
+class LoadFromRemoteUseCaseTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
