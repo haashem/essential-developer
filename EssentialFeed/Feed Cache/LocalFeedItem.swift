54f63439e68e60adc50011411563f6e648f54350
Hashem <hashemp206@yahoo.com>
2020-08-03 11:13:43 +0400

    Move 'LocalFeedItem' to its own file

---
+++ EssentialFeed/Feed Cache/LocalFeedItem.swift
@@ -0,0 +1,24 @@
+//
+//  LocalFeedItem.swift
+//  EssentialFeed
+//
+//  Created by Hashem Aboonajmi on 8/3/20.
+//  Copyright © 2020 Hashem Aboonajmi. All rights reserved.
+//
+
+import Foundation
+
+public struct LocalFeedItem: Equatable {
+    public let id: UUID
+    public let description: String?
+    public let location: String?
+    public let imageURL: URL
+    
+    public init(id: UUID, description: String?, location: String?, imageURL: URL) {
+        self.id = id
+        self.description = description
+        self.location = location
+        self.imageURL = imageURL
+    }
+}
+
