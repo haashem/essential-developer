a2b7e5a03e08de67ed67021312ca09976be23b13
Hashem <hashemp206@yahoo.com>
2021-10-27 08:43:07 +0400

    The cache directory permission is different in the iOS simulator (you can delete the directory on the simulator, but not on the macOS target!). hence for delete use non writable cache

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FeedStoreSpecs.swift
@@ -130,7 +130,7 @@ extension FeedStoreSpecs where Self: XCTestCase {
            exp.fulfill()
        }
        
-       wait(for: [exp], timeout: 1.0)
+       wait(for: [exp], timeout: 3.0)
        return deletionError
     }
     
