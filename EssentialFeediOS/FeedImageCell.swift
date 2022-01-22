4b839b2e6cc71bee750815daa3179dba9a34659a
Hashem <hashemp206@yahoo.com>
2022-01-22 18:31:39 +0400

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Hashem Abounajmi on 22/01/2022.
+//  Copyright © 2022 Hashem Aboonajmi. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
