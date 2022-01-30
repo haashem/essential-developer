c596dcfb347d0fa3c068f602d02b316aab4e782e
Hashem <hashemp206@yahoo.com>
2022-01-30 11:56:56 +0400

    Add `FeedPresenter` holding feed presentation logic (loading state and loaded feed)

---
+++ EssentialFeediOS/Feed Presentation/FeedPresenter.swift
@@ -0,0 +1,40 @@
+//
+//  FeedPresenter.swift
+//  EssentialFeediOS
+//
+//  Created by Hashem Abounajmi on 30/01/2022.
+//  Copyright © 2022 Hashem Aboonajmi. All rights reserved.
+//
+
+import EssentialFeed
+
+protocol FeedLoadingView: AnyObject {
+    func display(isLoading: Bool)
+}
+
+protocol FeedView {
+    func display(feed: [FeedImage])
+}
+
+final class FeedPresenter {
+    
+    typealias Observer<T> = (T) -> Void
+    private let feedLoader: FeedLoader
+    
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var feedView: FeedView?
+    weak var feedLoadingView: FeedLoadingView?
+    
+    func loadFeed() {
+        feedLoadingView?.display(isLoading: true)
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.feedView?.display(feed: feed)
+            }
+            self?.feedLoadingView?.display(isLoading: false)
+        }
+    }
+}
--- EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -8,27 +8,29 @@
 
 import UIKit
 
-final class FeedRefreshViewController: NSObject {
-    private(set) lazy var view = binded(UIRefreshControl())
+final class FeedRefreshViewController: NSObject, FeedLoadingView {
+    private(set) lazy var view = loadView()
     
-    private var viewModel: FeedViewModel
+    private var feedPresenter: FeedPresenter
     
-    init(viewModel: FeedViewModel) {
-        self.viewModel = viewModel
+    init(presenter: FeedPresenter) {
+        self.feedPresenter = presenter
+    }
+    
+    func display(isLoading: Bool) {
+        if isLoading {
+            view.beginRefreshing()
+        } else {
+            view.endRefreshing()
+        }
     }
     
     @objc func refresh() {
-        viewModel.loadFeed()
+        feedPresenter.loadFeed()
     }
     
-    private func binded(_ view: UIRefreshControl) -> UIRefreshControl {
-        viewModel.onLoadingStateChange = { [weak view] isLoading in
-            if isLoading {
-                view?.beginRefreshing()
-            } else {
-                view?.endRefreshing()
-            }
-        }
+    private func loadView() -> UIRefreshControl {
+        let view = UIRefreshControl()
         view.addTarget(self, action: #selector(refresh), for: .valueChanged)
         return view
     }
