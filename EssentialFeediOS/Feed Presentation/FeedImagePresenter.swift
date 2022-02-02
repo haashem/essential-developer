f2b76d1aacfd35a1dd447a7d436d716ab61e9cd1
Hashem <hashemp206@yahoo.com>
2022-02-02 19:22:18 +0400

    Add FeedImagePresenter holding FeedImage presentation logic (including image loading state)

---
+++ EssentialFeediOS/Feed Presentation/FeedImagePresenter.swift
@@ -0,0 +1,47 @@
+//
+//  FeedImagePresenter.swift
+//  EssentialFeediOS
+//
+//  Created by Hashem Abounajmi on 01/02/2022.
+//  Copyright © 2022 Hashem Aboonajmi. All rights reserved.
+//
+
+import Foundation
+import EssentialFeed
+
+protocol FeedImageView {
+    associatedtype Image
+    func display(_ model: FeedImageViewModel<Image>)
+}
+
+protocol FeedImageViewLoading {
+    func display(isLoading: Bool)
+}
+
+final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
+    private let view: View
+    private let imageTransformer: (Data) -> Image?
+    
+    init(view: View, imageTransformer: @escaping (Data) -> Image?) {
+        self.view = view
+        self.imageTransformer = imageTransformer
+    }
+    
+    func didStartLoadingImageData(for model: FeedImage) {
+        self.view.display(FeedImageViewModel(description: model.description, location: model.location, image: nil, isLoading: true, shouldRetry: false))
+    }
+    
+    private struct InvalidImageDataError: Error {}
+    
+    func didFinishLoadingImageData(with data: Data, for model: FeedImage) {
+        
+        guard let image = imageTransformer(data) else {
+            return didFinishLoadingImageData(with: InvalidImageDataError(), for: model)
+        }
+        view.display(FeedImageViewModel(description: model.description, location: model.location, image: image, isLoading: false, shouldRetry: false))
+    }
+    
+    func didFinishLoadingImageData(with error: Error, for model: FeedImage) {
+        view.display(FeedImageViewModel(description: model.description, location: model.location, image: nil, isLoading: false, shouldRetry: true))
+    }
+}
