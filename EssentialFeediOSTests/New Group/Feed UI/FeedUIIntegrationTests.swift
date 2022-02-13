47167d38b589bdd3ee0c1d7f520f4e26ce4fdc17
Hashem <hashemp206@yahoo.com>
2022-02-13 18:47:50 +0400

    Does not render load feed error on load

--- EssentialFeediOSTests/New Group/Feed UI/FeedUIIntegrationTests.swift
+++ EssentialFeediOSTests/New Group/Feed UI/FeedUIIntegrationTests.swift
@@ -84,6 +84,13 @@ final class FeedUIIntegrationTests: XCTestCase {
         assertThat(sut, isRendering: [image0])
     }
     
+    func test_errorView_doesntRenderOnLoad() {
+        let (sut, _) = makeSUT()
+        sut.loadViewIfNeeded()
+                
+        XCTAssertEqual(sut.errorMessage, nil)
+    }
+    
     func test_feedImageView_loadsImageURLwhenVisible() {
         let image0 = makeImage(url: URL(string: "https://url-0.com")!);
         let image1 = makeImage(url: URL(string: "https://url-1.com")!);
