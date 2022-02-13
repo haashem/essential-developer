ab16d6d67885239f3b4cc47a0adcbea55c4b561c
Hashem <hashemp206@yahoo.com>
2022-02-13 21:26:20 +0400

    Extract refreshing logic to an extension

---
+++ EssentialFeediOS/Feed UI/Views/Helpers/UIRefreshControl+Helpers.swift
@@ -0,0 +1,15 @@
+//
+//  UIRefreshControl+Helpers.swift
+//  EssentialFeediOS
+//
+//  Created by Hashem Abounajmi on 13/02/2022.
+//  Copyright © 2022 Hashem Aboonajmi. All rights reserved.
+//
+
+import UIKit
+
+extension UIRefreshControl {
+    func update(isRefreshing: Bool) {
+        isRefreshing ? beginRefreshing() : endRefreshing()
+    }
+}
