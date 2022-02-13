7042ea4a70b8f9dac1a87028991e2f118dc80d6f
Hashem <hashemp206@yahoo.com>
2022-02-13 19:57:51 +0400

    Move `ErrorView` to separate file

---
+++ EssentialFeediOS/Feed UI/Views/ErrorView.swift
@@ -0,0 +1,14 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Hashem Abounajmi on 13/02/2022.
+//  Copyright © 2022 Hashem Aboonajmi. All rights reserved.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
+
