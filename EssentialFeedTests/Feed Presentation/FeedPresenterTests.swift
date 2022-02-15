10323bbc00f2fd4cf91eae30dcaf5f7d87eb9bde
Hashem <hashemp206@yahoo.com>
2022-02-15 18:45:43 +0400

    `FeedPresenter` displays feed and stops loading on `didFinishLoadingFeed`

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import EssentialFeed
 
 struct FeedErrorViewModel {
     let message: String?
@@ -28,12 +29,22 @@ protocol FeedLoadingView {
     func display(_ viewModel: FeedLoadingViewModel)
 }
 
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
+
+protocol FeedView {
+    func display(_ viewModel: FeedViewModel)
+}
+
 
 final class FeedPresenter {
+    private let feedView: FeedView
     let errorView: FeedErrorView
     let loadingView: FeedLoadingView
     
-    init(loadingView: FeedLoadingView, errorView: FeedErrorView) {
+    init(feedView: FeedView, loadingView: FeedLoadingView, errorView: FeedErrorView) {
+        self.feedView = feedView
         self.errorView = errorView
         self.loadingView = loadingView
     }
@@ -42,13 +53,18 @@ final class FeedPresenter {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
     }
+    
+    func didFinishLoadingFeed(with feed: [FeedImage]) {
+        feedView.display(FeedViewModel(feed: feed))
+        loadingView.display(FeedLoadingViewModel(isLoading: false))
+    }
 }
 
 class FeedPresenterTests: XCTestCase {
     
     func test_init_doesntSendMessagesToView() {
         let (_, view) = makeSUT()
-        _ = FeedPresenter(loadingView: view, errorView: view)
+        _ = FeedPresenter(feedView: view, loadingView: view, errorView: view)
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
@@ -59,21 +75,31 @@ class FeedPresenterTests: XCTestCase {
         XCTAssertEqual(view.messages, [.display(errorMessage: .none), .display(isLoading: true)])
     }
     
+    func test_didFinishLoadingFeed_displaysFeedMessageAndStopsLoading()
+    {
+        let (sut, view) = makeSUT()
+        let feed = uniqueImageFeed().models
+        sut.didFinishLoadingFeed(with: feed)
+        
+        XCTAssertEqual(view.messages, [.display(feed: feed), .display(isLoading: true)])
+    }
+    
     // MARK: Helper
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedPresenter(loadingView: view, errorView: view)
+        let sut = FeedPresenter(feedView: view, loadingView: view, errorView: view)
         trackForMemoryLeaks(view)
         trackForMemoryLeaks(sut)
         return (sut, view)
     }
     
-    private class ViewSpy: FeedErrorView, FeedLoadingView {
+    private class ViewSpy: FeedErrorView, FeedLoadingView, FeedView {
         
         enum Message: Hashable {
             case display(errorMessage: String?)
             case display(isLoading: Bool)
+            case display(feed: [FeedImage])
         }
         
         private(set) var messages = Set<Message>()
@@ -84,6 +110,10 @@ class FeedPresenterTests: XCTestCase {
         
         func display(_ viewModel: FeedLoadingViewModel) {
             messages.insert(.display(isLoading: true))
+        }
+        
+        func display(_ viewModel: FeedViewModel) {
+            messages.insert(.display(feed: viewModel.feed))
         }
     }
 }
