46ffee81b4cb483873e7995887c039831f0ecaee
Hashem <hashemp206@yahoo.com>
2022-03-05 13:09:11 +0400

    Move `FeedImageDataLoaderCacheDecorator` to production

--- EssentialApp/EssentialAppTests/FeedImageLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageLoaderCacheDecoratorTests.swift
@@ -7,26 +7,7 @@
 
 import XCTest
 import EssentialFeed
-
-class FeedImageDataLoaderWithCacheDecorator: FeedImageDataLoader {
-    
-    private let decoratee: FeedImageDataLoader
-    private let cache: FeedImageDataCache
-    
-    init(decoratee: FeedImageDataLoader, cache: FeedImageDataCache) {
-        self.decoratee = decoratee
-        self.cache = cache
-    }
-    
-    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
-        return decoratee.loadImageData(from: url) { [weak self] result in
-            completion(result.map { imageData in
-                self?.cache.save(imageData, for: url){ _ in }
-                return imageData
-            })
-        }
-    }
-}
+import EssentialApp
 
 class FeedImageLoaderCacheDecoratorTests: XCTestCase, FeedImageDataLoaderTestCase {
   
