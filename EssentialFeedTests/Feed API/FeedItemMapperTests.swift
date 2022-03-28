a058a5672d6c12623e896c230441b9f70b47b720
Hashem <hashemp206@yahoo.com>
2022-03-28 21:36:41 +0400

    Remove unnecessary throws

--- EssentialFeedTests/Feed API/FeedItemMapperTests.swift
+++ EssentialFeedTests/Feed API/FeedItemMapperTests.swift
@@ -20,7 +20,7 @@ class FeedItemMapperTests: XCTestCase {
         }
     }
     
-    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() throws {
+    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
         let invalidJSON = Data("invalid json".utf8)
         
         XCTAssertThrowsError(try FeedItemMapper.map(invalidJSON, from: HTTPURLResponse(statusCode: 200)))
