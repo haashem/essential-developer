a83839d1c6b67348e0c3084c79b9fc3057ec6140
Hashem <hashemp206@yahoo.com>
2022-03-29 18:15:17 +0400

    Remove unused RemoteLoader

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -68,4 +68,3 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 }
 
-extension RemoteLoader: FeedLoader where Resource == [FeedImage] {}
--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -49,17 +49,21 @@ class EssentialFeedAPIEndToEndTests: XCTestCase {
     
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> FeedLoader.Result? {
         
-        let loader = RemoteLoader(url: feedTestServerURL, client: ephemeralClient(), mapper: FeedItemMapper.map)
-        
-        trackForMemoryLeaks(loader, file: file, line:  line)
-        
+        let client = ephemeralClient()
+       
         let exp = expectation(description: "wait for load completion")
         var receivedResult: FeedLoader.Result?
         
-        loader.load() { result in
-            receivedResult = result
+        client.get(from: feedTestServerURL, completion: { result in
+            receivedResult = result.flatMap{ (data, response) in
+                do {
+                    return .success(try FeedItemMapper.map(data, from: response))
+                } catch {
+                    return .failure(error)
+                }
+            }
             exp.fulfill()
-        }
+        })
         
         wait(for: [exp], timeout: 10.0)
         
