0d0cb6e8b00b0ea5b622f0356f40d154ccbebec2
Hashem <hashemp206@yahoo.com>
2022-05-29 12:44:51 +0400

    Render comments

--- EssentialApp/EssentialApp/CommentsUIComposer.swift
+++ EssentialApp/EssentialApp/CommentsUIComposer.swift
@@ -14,25 +14,51 @@ final public class CommentsUIComposer {
     
     private init() {}
     
-    public static func commentsComposedWith(commentsLoader: @escaping () -> AnyPublisher<[FeedImage], Error>) -> ListViewController {
+    public static func commentsComposedWith(commentsLoader: @escaping () -> AnyPublisher<[ImageComment], Error>) -> ListViewController {
         
-        let presentationAdapater = LoadResourcePresentationAdapater<[FeedImage], FeedViewAdapter>(loader: {
+        let presentationAdapater = LoadResourcePresentationAdapater<[ImageComment], CommentsViewAdapter>(loader: {
             commentsLoader().dispatchToMainQueue()
         })
         
-        let feedController = makeFeedViewController(title: ImageCommentsPresenter.title)
-        feedController.onRefresh = presentationAdapater.loadResource
+        let commentsController = makeCommentsViewController(title: ImageCommentsPresenter.title)
+        commentsController.onRefresh = presentationAdapater.loadResource
         
-        presentationAdapater.presenter = LoadResourcePresenter(resourceView: FeedViewAdapter(controller: feedController, imageLoader: { _ in Empty<Data, Error>().eraseToAnyPublisher() }), loadingView: WeakRefVirtualProxy(feedController), errorView: WeakRefVirtualProxy(feedController), mapper: FeedPresenter.map)
+        presentationAdapater.presenter = LoadResourcePresenter(resourceView: CommentsViewAdapter(controller: commentsController), loadingView: WeakRefVirtualProxy(commentsController), errorView: WeakRefVirtualProxy(commentsController), mapper: { ImageCommentsPresenter.map(comments: $0) })
         
-        return feedController
+        return commentsController
     }
     
-    private static func makeFeedViewController(title: String) -> ListViewController {
+    private static func makeCommentsViewController(title: String) -> ListViewController {
         let bundle = Bundle(for: ListViewController.self)
-        let storybaord = UIStoryboard(name: "Feed", bundle: bundle.self)
-        let feedController = storybaord.instantiateInitialViewController() as! ListViewController
-        feedController.title = title
-        return feedController;
+        let storybaord = UIStoryboard(name: "ImageComments", bundle: bundle.self)
+        let controller = storybaord.instantiateInitialViewController() as! ListViewController
+        controller.title = title
+        return controller;
     }
+}
+
+final class CommentsViewAdapter: ResourceView {
+    
+    private weak var controller: ListViewController?
+    
+    init(controller: ListViewController) {
+        self.controller = controller
+    }
+    
+    func display(_ viewModel: ImageCommentsViewModel) {
+        controller?.display(viewModel.comments.map { viewModel in
+            CellController(id: viewModel, ImageCommentCellController(model: viewModel))
+        })
+    }
+}
+
+final class DataSource: NSObject, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
 }
--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -14,6 +14,51 @@ extension ListViewController {
         refreshControl?.simulatePullToRefresh()
     }
     
+    func simulateErrorViewTap() {
+        errorView.simulateTap()
+    }
+    
+    var errorMessage: String? {
+        return errorView.message
+    }
+    
+    var isShowingLoadingIndicator: Bool {
+        return refreshControl?.isRefreshing == true
+    }
+}
+
+extension ListViewController {
+    
+    func numberOfRenderedComments() -> Int {
+        tableView.numberOfSections == 0 ? 0 : tableView.numberOfRows(inSection: commentsSection)
+    }
+    
+    func commentMessage(at row: Int) -> String? {
+        commentView(at: row)?.messageLabel.text
+    }
+    
+    func commentDate(at row: Int) -> String? {
+        commentView(at: row)?.dateLabel.text
+    }
+    
+    func commentUsername(at row: Int) -> String? {
+        commentView(at: row)?.usernameLabel.text
+    }
+    
+    private func commentView(at row: Int) -> ImageCommentCell? {
+        guard numberOfRenderedComments() > row else { return nil }
+        let ds = tableView?.dataSource
+        let index = IndexPath(row: row, section: feedImagesSection)
+        
+        return ds?.tableView(tableView, cellForRowAt: index) as? ImageCommentCell
+    }
+    
+    private var commentsSection: Int {
+        return 0
+    }
+}
+
+extension ListViewController {
     @discardableResult
     func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell?  {
         return feedImageView(at: index) as? FeedImageCell
@@ -46,18 +91,6 @@ extension ListViewController {
     
     func renderedFeedImageData(at index: Int = 0) -> Data? {
         return simulateFeedImageViewNotVisible(at: 0)?.renderedImage
-    }
-    
-    func simulateErrorViewTap() {
-        errorView.simulateTap()
-    }
-    
-    var errorMessage: String? {
-        return errorView.message
-    }
-    
-    var isShowingLoadingIndicator: Bool {
-        return refreshControl?.isRefreshing == true
     }
     
     func numberOfRenderedFeedImageViews() -> Int {
